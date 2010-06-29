dc09acc941cc5951c08a971d9bafd5541311269a
Gary King <gking@nvidia.com>
2010-06-28 19:50:47 -0700

    nvidia/build: branch nv-droid build
    Change-Id: I8e9f0822e40afb23aff3ca5e3e829fff5d9fe66f

---
+++ defaults.mk
@@ -0,0 +1,37 @@
+
+# Grab name of the makefile to depend on it
+ifneq ($(PREV_LOCAL_PATH),$(LOCAL_PATH))
+NVIDIA_MAKEFILE := $(lastword $(filter-out $(lastword $(MAKEFILE_LIST)),$(MAKEFILE_LIST)))
+PREV_LOCAL_PATH := $(LOCAL_PATH)
+endif
+include $(CLEAR_VARS)
+
+# Build variables common to all nvidia modules
+
+LOCAL_C_INCLUDES += $(TEGRA_ROOT)/include
+LOCAL_C_INCLUDES += $(TEGRA_ROOT)/drivers/hwinc
+
+ifeq ($(TARGET_BUILD_TYPE),debug)
+LOCAL_CFLAGS += -DNV_DEBUG=1
+# TODO: fix source that relies on these
+LOCAL_CFLAGS += -DDEBUG
+LOCAL_CFLAGS += -D_DEBUG
+else
+LOCAL_CFLAGS += -DNV_DEBUG=0
+endif
+LOCAL_CFLAGS += -DNV_IS_AVP=0
+LOCAL_CFLAGS += -DNV_BUILD_STUBS=1
+
+LOCAL_PRELINK_MODULE := false
+
+# clear nvidia local variables to defaults
+NVIDIA_CLEARED := true
+LOCAL_IDL_INCLUDES := $(TEGRA_ROOT)/include
+LOCAL_IDLFLAGS :=
+LOCAL_NVIDIA_STUBS :=
+LOCAL_NVIDIA_DISPATCHERS :=
+LOCAL_NVIDIA_SHADERS :=
+LOCAL_NVIDIA_GEN_SHADERS :=
+LOCAL_NVIDIA_PKG :=
+LOCAL_NVIDIA_PKG_DISPATCHER :=
+LOCAL_NVIDIA_EXPORTS :=
---
+++ nv_install.sh
@@ -0,0 +1,24 @@
+# Enable abort on errors
+set -e 
+
+# Parse any special options given
+link_args=
+if [ $1 = "-l" ]; then
+    shift;
+    link_args=-l;
+fi
+
+# Need to have at least 2 args left
+if [ $# -lt 2 ]; then
+    echo $0: too few args provided;
+    exit 1;
+fi
+
+# Extract out directory name (last arg), while remembering the args we need to pass to cp
+copy_args=$*
+while [ $# -ge 2 ]; do shift; done
+dir=$1
+
+# Create the target directory, and copy or hard link the files
+mkdir -p $dir
+cp -fu $copy_args
