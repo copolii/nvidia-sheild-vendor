dbbdc77e691d211f7c02ff571a61659ea3273b59
Gary King <gking@nvidia.com>
2010-07-09 16:24:25 -0700

    build TEGRA_ROOT tree if the envar is set
    Change-Id: If5748585588ff6cbbf744f2864a6efc1c866dc91

--- Android.mk
+++ Android.mk
@@ -2,6 +2,9 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
+ifneq ($(TEGRA_ROOT),)
+include $(TEGRA_ROOT)/Android.mk
+else
 file := $(HOST_OUT_EXECUTABLES)/nvflash$(HOST_EXECUTABLE_SUFFIX)
 ALL_PREBUILT += $(file)
 $(file) : $(LOCAL_PATH)/$(notdir $(file)) | $(ACP)
@@ -10,3 +13,4 @@ $(file) : $(LOCAL_PATH)/$(notdir $(file)) | $(ACP)
 PRODUCT_COPY_FILES += $(LOCAL_PATH)/$(TARGET_DEVICE)/bootloader.bin:bootloader.bin
 PRODUCT_COPY_FILES += $(LOCAL_PATH)/$(TARGET_DEVICE)/flash.cfg:flash.cfg
 PRODUCT_COPY_FILES += $(LOCAL_PATH)/$(TARGET_DEVICE)/flash.bct:flash.bct
+endif
