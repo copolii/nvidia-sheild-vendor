b170ac9da7f50695f0e5420b7c0b96a20f2f26a3
Scott Williams <scwilliams@nvidia.com>
2011-01-20 13:11:24 -0800

    [vendor/nvidia] Use DCC for Tegra3 bootloader debug console
    Bug 782198
    
    Change-Id: I9f7c0899ae94bdc02415f3a3dcce5e1cf182eaa1
    Reviewed-on: http://git-master/r/16449
    Reviewed-by: Scott Williams <scwilliams@nvidia.com>
    Tested-by: Scott Williams <scwilliams@nvidia.com>

--- cardhu/cardhu.sh
+++ cardhu/cardhu.sh
@@ -10,7 +10,7 @@ export FASTBOOT_VID=0x955
 echo "Setting up NvFlash BCT for Cardhu with DDR3 SDRAM......"
 cp $TEGRA_ROOT/../customers/nvidia/cardhu/nvflash/cardhu_13Mhz_H5TQ1G83BFR-H9C_13Mhz_1GB_emmc_H26M42001EFR_x8.bct $TOP/$OUTDIR/flash.bct
 cp $TEGRA_ROOT/../customers/nvidia/cardhu/nvflash/android_fastboot_emmc_full.cfg $TOP/$OUTDIR/flash.cfg
-export NVFLASH_ODM_DATA=0x400c0105
+export NVFLASH_ODM_DATA=0x40080105
 
 cp $TOP/$OUTDIR/obj/EXECUTABLES/bootloader_intermediates/bootloader.bin $TOP/$OUTDIR/bootloader.bin
 
