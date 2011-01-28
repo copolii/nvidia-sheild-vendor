61e18c117f3295e91daafb09807efbca2dd8c850
Scott Williams <scwilliams@nvidia.com>
2011-01-28 00:54:34 -0800

    vendor: nvidia: Enable 2GB SDRAM for cardhu
    Change-Id: I0d3216e6684afb7680385a6355db518d0527ecd5
    Reviewed-on: http://git-master/r/17462
    Tested-by: Scott Williams <scwilliams@nvidia.com>
    Reviewed-by: Jonathan Mayo <jmayo@nvidia.com>
    Reviewed-by: Krishna Reddy <vdumpa@nvidia.com>
    Reviewed-by: Scott Williams <scwilliams@nvidia.com>

--- cardhu/cardhu.sh
+++ cardhu/cardhu.sh
@@ -1,17 +1,15 @@
 # NVIDIA Tegra3 "Cardhu" development system
-echo "DEBUG: Entering $TOP/vendor/nvidia/build/cardhu/cardhu.sh"
-
 local OUTDIR=$(get_build_var PRODUCT_OUT)
 echo "DEBUG: PRODUCT_OUT = $OUTDIR"
 
 # setup FASTBOOT VENDOR ID
 export FASTBOOT_VID=0x955
 
-echo "Setting up NvFlash BCT for Cardhu with DDR3 SDRAM......"
+# Set ODM_DATA for 2GB SDRAM
+export NVFLASH_ODM_DATA=0x80080105
+
+echo "Setting up NvFlash BCT for Cardhu with 2GB DDR3 SDRAM......"
 cp $TEGRA_ROOT/../customers/nvidia/cardhu/nvflash/cardhu_12Mhz_H5TC2G83BFR_333Mhz_1GB_emmc_SDIN5C2-16G_x8.bct $TOP/$OUTDIR/flash.bct
 cp $TEGRA_ROOT/../customers/nvidia/cardhu/nvflash/android_fastboot_emmc_full.cfg $TOP/$OUTDIR/flash.cfg
-export NVFLASH_ODM_DATA=0x40080105
-
 cp $TOP/$OUTDIR/obj/EXECUTABLES/bootloader_intermediates/bootloader.bin $TOP/$OUTDIR/bootloader.bin
 
-echo "DEBUG: Leaving $TOP/vendor/nvidia/build/cardhu/cardhu.sh"
