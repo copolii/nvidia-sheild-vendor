04ad1be8c9f218410117bd3bbb93264f7aa28e88
Scott Williams <scwilliams@nvidia.com>
2011-01-29 15:41:51 -0800

    tegra: Enable 533 MHz BCT for cardhu
    Change-Id: Ie5f1f14cde7cc15111d8596bc900d6336fd8667d
    Reviewed-on: http://git-master/r/17588
    Tested-by: Scott Williams <scwilliams@nvidia.com>
    Reviewed-by: Andrew Howe <ahowe@nvidia.com>
    Tested-by: Andrew Howe <ahowe@nvidia.com>
    Reviewed-by: Scott Williams <scwilliams@nvidia.com>

--- cardhu/cardhu.sh
+++ cardhu/cardhu.sh
@@ -8,8 +8,8 @@ export FASTBOOT_VID=0x955
 # Set ODM_DATA for 2GB SDRAM
 export NVFLASH_ODM_DATA=0x80080105
 
-echo "Setting up NvFlash BCT for Cardhu with 2GB DDR3 SDRAM......"
-cp $TEGRA_ROOT/../customers/nvidia/cardhu/nvflash/cardhu_12Mhz_H5TC2G83BFR_333Mhz_1GB_emmc_SDIN5C2-16G_x8.bct $TOP/$OUTDIR/flash.bct
+echo "Setting up NvFlash BCT for Cardhu with 2GB 533MHz DDR3 SDRAM......"
+cp $TEGRA_ROOT/../customers/nvidia/cardhu/nvflash/E1187_Hynix_2GB_H5TC2G83BFR-PBA_533MHz_20110126_0515.bct $TOP/$OUTDIR/flash.bct
 cp $TEGRA_ROOT/../customers/nvidia/cardhu/nvflash/android_fastboot_emmc_full.cfg $TOP/$OUTDIR/flash.cfg
 cp $TOP/$OUTDIR/obj/EXECUTABLES/bootloader_intermediates/bootloader.bin $TOP/$OUTDIR/bootloader.bin
 
