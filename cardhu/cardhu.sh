440f572af3f3c6ebf11f6f888cf8e77bd5f36319
Scott Williams <scwilliams@nvidia.com>
2011-02-07 08:44:03 -0800

    vendor: nvidia: Update BCT frequency in echo message
    Change-Id: I6d834f44d6f9c8f961b804aaac2411e905e9a738
    Reviewed-on: http://git-master/r/18581
    Reviewed-by: Scott Williams <scwilliams@nvidia.com>
    Tested-by: Scott Williams <scwilliams@nvidia.com>

--- cardhu/cardhu.sh
+++ cardhu/cardhu.sh
@@ -8,7 +8,7 @@ export FASTBOOT_VID=0x955
 # Set ODM_DATA for 2GB SDRAM
 export NVFLASH_ODM_DATA=0x80080105
 
-echo "Setting up NvFlash BCT for Cardhu with 2GB 533MHz DDR3 SDRAM......"
+echo "Setting up NvFlash BCT for Cardhu with 2GB 677MHz DDR3 SDRAM......"
 cp $TEGRA_ROOT/../customers/nvidia/cardhu/nvflash/E1187_Hynix_2GB_H5TC2G83BFR-PBA_667MHz_20110203_2316.bct $TOP/$OUTDIR/flash.bct
 cp $TEGRA_ROOT/../customers/nvidia/cardhu/nvflash/android_fastboot_emmc_full.cfg $TOP/$OUTDIR/flash.cfg
 cp $TOP/$OUTDIR/obj/EXECUTABLES/bootloader_intermediates/bootloader.bin $TOP/$OUTDIR/bootloader.bin
