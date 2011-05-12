1ad1275c4b35ea0a191850ceb6656fcc7dfefe5a
Pradeep Goudagunta <pgoudagunta@nvidia.com>
2011-05-12 17:08:56 +0530

    enterprise: odm-data: update odm for debug_uart selection
    updated odm-data to select debug_uart as uart4 according to new odm
    options.
    
    Bug 814271
    Bug 822432
    
    Change-Id: I5dc1589a70683f02af28daf1d77ca16f674d6f83
    Reviewed-on: http://git-master/r/31347
    Tested-by: Pradeep Goudagunta <pgoudagunta@nvidia.com>
    Reviewed-by: Laxman Dewangan <ldewangan@nvidia.com>

--- enterprise/enterprise.sh
+++ enterprise/enterprise.sh
@@ -6,7 +6,7 @@ echo "DEBUG: PRODUCT_OUT = $OUTDIR"
 export FASTBOOT_VID=0x955
 
 # Set ODM_DATA for 2GB SDRAM
-export NVFLASH_ODM_DATA=0x20080105
+export NVFLASH_ODM_DATA=0x20098105
 
 echo "Setting up NvFlash BCT for Enterprise with 512MB 533MHz DDR3 SDRAM......"
 cp $TEGRA_ROOT/../customers/nvidia-partner/enterprise/nvflash/E1205_Hynix_512MB_KMMLL0000QM-B503_300MHz_20110408.bct $TOP/$OUTDIR/flash.bct
