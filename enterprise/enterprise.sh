60ae08c9f1fc2c803807128bbd036a9e54d24fa0
Ray Poudrier <rapoudrier@nvidia.com>
2011-07-11 20:27:26 -0700

    vendor: nvidia: enterprise: update handling of bct
    Use an environment variable to copy the correct bct
    ENTERPRISE_A01=1 will use older A01 bct
    
    Change-Id: Ibaa9b34556ca8ad50fdc1ad0c36f757965cfac47
    Reviewed-on: http://git-master/r/40538
    Reviewed-by: Chih-Lung Huang <lhuang@nvidia.com>
    Reviewed-by: Karan Jhavar <kjhavar@nvidia.com>
    Tested-by: Chih-Lung Huang <lhuang@nvidia.com>
    Reviewed-by: Yu-Huan Hsu <yhsu@nvidia.com>
    Reviewed-by: Thomas Cherry <tcherry@nvidia.com>

--- enterprise/enterprise.sh
+++ enterprise/enterprise.sh
@@ -8,8 +8,13 @@ export FASTBOOT_VID=0x955
 # Set ODM_DATA for 768MB SDRAM
 export NVFLASH_ODM_DATA=0x30098105
 
-echo "Setting up NvFlash BCT for Enterprise with 512MB 533MHz DDR3 SDRAM......"
-cp $TEGRA_ROOT/../customers/nvidia-partner/enterprise/nvflash/E1205_Hynix_512MB_KMMLL0000QM-B503_300MHz_20110408.bct $TOP/$OUTDIR/flash.bct
+if [ "$ENTERPRISE_A01" ]
+then
+    cp $TOP/$OUTDIR/flash_a01.bct $TOP/$OUTDIR/flash.bct
+else
+    cp $TOP/$OUTDIR/flash_a02.bct $TOP/$OUTDIR/flash.bct
+fi
+
 cp $TEGRA_ROOT/../customers/nvidia-partner/enterprise/nvflash/android_fastboot_emmc_full.cfg $TOP/$OUTDIR/flash.cfg
 cp $TOP/$OUTDIR/obj/EXECUTABLES/bootloader_intermediates/bootloader.bin $TOP/$OUTDIR/bootloader.bin
 
