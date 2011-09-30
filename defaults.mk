12110f4caa549955c4d90989cb3f47dfa41a77fb
Khong Jye Liew <kliew@nvidia.com>
2011-09-30 16:24:29 -0700

    build: Add SOC family to standard include search path
    Change-Id: Idf92d545566ff25f0d97e5254f28a68981db5c83
    Reviewed-on: http://git-master/r/55585
    Reviewed-by: Scott Williams <scwilliams@nvidia.com>
    Tested-by: Scott Williams <scwilliams@nvidia.com>
    Original: fd2357a79a450458a57aa8653294264109451c14

--- defaults.mk
+++ defaults.mk
@@ -14,6 +14,7 @@ LOCAL_C_INCLUDES += $(TEGRA_TOP)/core/drivers/hwinc
 ifneq (,$(findstring core-private,$(LOCAL_PATH)))
 LOCAL_C_INCLUDES += $(TEGRA_TOP)/core-private/include
 LOCAL_C_INCLUDES += $(TEGRA_TOP)/core-private/drivers/hwinc
+LOCAL_C_INCLUDES += $(TEGRA_TOP)/core-private/drivers/hwinc/$(TARGET_TEGRA_FAMILY)
 endif
 
 ifneq (,$(findstring tests,$(LOCAL_PATH)))
