40adfda03bec66e56ee0b8b0a3a1b3df14c92b2a
Sridhar Lavu <slavu@nvidia.com>
2011-10-19 19:50:07 -0700

    build: add PLATFORM_IS_HONEYCOMB support
    - check PLATFORM_VERSION for 2.% and 3.% and set
      PLATFORM_IS_HONEYCOMB and PLATFORM_IS_GINGERBREAD
    - add PLATFORM_IS_HONEYCOMB to TEGRA_CFLAGS
    
    Change-Id: I595013087fa4be5e43a4688cfb70a5360e1f0c0e
    Reviewed-on: http://git-master/r/59240
    Reviewed-by: Oskari Jaaskelainen <oskarij@nvidia.com>
    Reviewed-by: Prajakta Gudadhe <pgudadhe@nvidia.com>
    Reviewed-by: Michael I Gold <gold@nvidia.com>
    Reviewed-by: Sridhar Lavu <slavu@nvidia.com>
    Tested-by: Sridhar Lavu <slavu@nvidia.com>

--- detectversion.mk
+++ detectversion.mk
@@ -1,3 +1,7 @@
-ifneq (, $(findstring 2.3., $(PLATFORM_VERSION)))
-PLATFORM_IS_GINGERBREAD := YES
+ifneq (,$(filter 2.%,$(PLATFORM_VERSION)))
+  PLATFORM_IS_GINGERBREAD := YES
+else ifneq (,$(filter 3.%,$(PLATFORM_VERSION)))
+  PLATFORM_IS_HONEYCOMB := YES
+else
+  $(warning PLATFORM_VERSION = $(PLATFORM_VERSION) is unknown))
 endif
