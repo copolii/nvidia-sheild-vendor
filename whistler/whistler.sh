1306aa52e174d99ce4090afa700b11742e660827
Jay Cheng <jacheng@nvidia.com>
2011-11-16 10:00:00 -0800

    tegra: whistler: nvflash: add AP25 A04 bct option
    Support AP25-A04-1GB module board
    
    Change-Id: I6d7c45cd7eecee76fa830ed60d60d388d3f5d796
    Signed-off-by: Jay Cheng <jacheng@nvidia.com>
    Reviewed-on: http://git-master/r/59690
    Reviewed-by: Bharat Nihalani <bnihalani@nvidia.com>

--- whistler/whistler.sh
+++ whistler/whistler.sh
@@ -1,4 +1,14 @@
 # NVIDIA Tegra2 "Whistler" development system
+OUTDIR=$(get_build_var PRODUCT_OUT)
+
+echo -n "Memory (1)512MB (2)1GB: "
+read -t 10 MEMORYSIZE
 
 # setup NVFLASH ODM Data
+cp $TOP/$OUTDIR/flash_512MB.bct $TOP/$OUTDIR/flash.bct
 export NVFLASH_ODM_DATA=0x2B080105
+
+if [ "$MEMORYSIZE" = "2" ]; then
+cp $TOP/$OUTDIR/flash_1GB.bct $TOP/$OUTDIR/flash.bct
+export NVFLASH_ODM_DATA=0x3B080105
+fi
