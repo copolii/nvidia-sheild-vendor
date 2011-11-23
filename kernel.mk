5e08d0117aecf35cfce9e0963881d284df3ab634
Jan-Petter Vainionpaa <jvainionpaa@nvidia.com>
2011-11-23 12:17:58 +0200

    build: Added dependency between kernel and system image
    Kernel build also includes some drivers as kernel modules which are
    packaged inside system image. Therefore, for incremental builds,
    dependency from kernel to installed system image must be introduced,
    so that recompilation of kernel automatically updates also the drivers
    in system image to be flashed to the device.
    
    Added also separate rule for creating needed directories, instead of
    creating them as a part of recipe. Listed these directories as "order
    only" dependencies for relevant targets. Related to this change,
    following variables were introduced (these paths are always absolute):
      - NV_KERNEL_INTERMEDIATES_DIR
      - NV_KERNEL_MODULES_TARGET_DIR
    
    Also modified the kernel-make macro to use NV_KERNEL_INTERMEDIATES_DIR
    variable.
    
    Above changes should now make kernel build to support the use of
    following syntax to relocate the output directory from default one
    $TOP/out to something else:
    
      mp OUT_DIR=/some/path/to/empty/directory dev
    
    Bug 798678
    
    Change-Id: I2541c2a2501bcf8f410a2dd809ad6a7031736eb1
    Reviewed-on: http://git-master/r/66424
    Reviewed-by: Varun Colbert <vcolbert@nvidia.com>
    Tested-by: Varun Colbert <vcolbert@nvidia.com>

--- kernel.mk
+++ kernel.mk
@@ -3,7 +3,13 @@
 #
 ifneq ($(TARGET_NO_KERNEL),true)
 
-KERNEL_PATH ?= kernel
+ifneq ($(TOP),.)
+$(error Kernel build assumes TOP == . i.e Android build has been started from TOP/Makefile )
+endif
+
+# Android build is started from the $TOP/Makefile, therefore $(CURDIR)
+# gives the absolute path to the TOP.
+KERNEL_PATH ?= $(CURDIR)/kernel
 
 #kernel_version := $(strip $(shell head $(KERNEL_PATH)/Makefile | \
 #	grep "SUBLEVEL =" | cut -d= -f2))
@@ -20,8 +26,21 @@ ifeq ($(wildcard $(KERNEL_PATH)/arch/arm/configs/$(TARGET_KERNEL_CONFIG)),)
     $(error Could not find kernel defconfig for board)
 endif
 
-_kernel_intermediates := $(TARGET_OUT_INTERMEDIATES)/KERNEL
-dotconfig := $(_kernel_intermediates)/.config
+# Always use absolute path for NV_KERNEL_INTERMEDIATES_DIR
+ifneq ($(filter /%, $(TARGET_OUT_INTERMEDIATES)),)
+NV_KERNEL_INTERMEDIATES_DIR := $(TARGET_OUT_INTERMEDIATES)/KERNEL
+else
+NV_KERNEL_INTERMEDIATES_DIR := $(CURDIR)/$(TARGET_OUT_INTERMEDIATES)/KERNEL
+endif
+
+dotconfig := $(NV_KERNEL_INTERMEDIATES_DIR)/.config
+
+# Always use absolute path for NV_KERNEL_MODULES_TARGET_DIR
+ifneq ($(filter /%, $(TARGET_OUT)),)
+NV_KERNEL_MODULES_TARGET_DIR := $(TARGET_OUT)/lib/modules
+else
+NV_KERNEL_MODULES_TARGET_DIR := $(CURDIR)/$(TARGET_OUT)/lib/modules
+endif
 
 KERNEL_EXTRA_ARGS=
 OS=$(shell uname)
@@ -47,19 +66,19 @@ ifeq ($(OS),Linux)
 endif
 
 # We should rather use CROSS_COMPILE=$(PRIVATE_TOPDIR)/$(TARGET_TOOLS_PREFIX).
+# Absolute paths used in all path variables.
 define kernel-make
 $(KERNEL_EXTRA_ENV) $(MAKE) -C $(PRIVATE_SRC_PATH) \
     ARCH=$(TARGET_ARCH) \
     CROSS_COMPILE=$(PRIVATE_TOPDIR)/prebuilt/$(HOSTTYPE)/toolchain/arm-eabi-4.4.3/bin/arm-eabi- \
-    O=$(PRIVATE_TOPDIR)/$(PRIVATE_KBUILD_OUT) $(KERNEL_EXTRA_ARGS) \
+    O=$(NV_KERNEL_INTERMEDIATES_DIR) $(KERNEL_EXTRA_ARGS) \
     $(if $(SHOW_COMMANDS),V=1)
 endef
 
-BUILT_KERNEL_TARGET := $(_kernel_intermediates)/arch/$(TARGET_ARCH)/boot/zImage
+BUILT_KERNEL_TARGET := $(NV_KERNEL_INTERMEDIATES_DIR)/arch/$(TARGET_ARCH)/boot/zImage
 
-$(dotconfig): $(KERNEL_PATH)/arch/$(TARGET_ARCH)/configs/$(TARGET_KERNEL_CONFIG)
+$(dotconfig): $(KERNEL_PATH)/arch/$(TARGET_ARCH)/configs/$(TARGET_KERNEL_CONFIG) | $(NV_KERNEL_INTERMEDIATES_DIR)
 	@echo "Kernel config"
-	@mkdir -p $(PRIVATE_KBUILD_OUT)
 	$(hide) $(kernel-make) $(TARGET_KERNEL_CONFIG)
 ifeq ($(SECURE_OS_BUILD),y)
 	@echo "SecureOS enabled kernel"
@@ -68,33 +87,32 @@ endif
 
 # TODO: figure out a way of not forcing kernel & module builds.
 # + in front of kernel-make will enable job control (parallelization).
-$(BUILT_KERNEL_TARGET): $(dotconfig) FORCE
+$(BUILT_KERNEL_TARGET): $(dotconfig) FORCE | $(NV_KERNEL_INTERMEDIATES_DIR)
 	@echo "Kernel build"
-	@mkdir -p $(PRIVATE_KBUILD_OUT)
 	+$(hide) $(kernel-make) zImage
 
 # This will add all kernel modules we build for inclusion the system
 # image - no blessing takes place.
-kmodules: $(BUILT_KERNEL_TARGET) FORCE
+kmodules: $(BUILT_KERNEL_TARGET) FORCE | $(NV_KERNEL_INTERMEDIATES_DIR) $(NV_KERNEL_MODULES_TARGET_DIR)
 	@echo "Kernel modules build"
 	+$(hide) $(kernel-make) modules
-	mkdir -p $(TARGET_OUT)/lib/modules
-	find $(PRIVATE_TOPDIR)/$(PRIVATE_KBUILD_OUT) -name "*.ko" -print0 | xargs -0 -IX cp -v X $(TARGET_OUT)/lib/modules/
+	find $(PRIVATE_TOPDIR)/$(NV_KERNEL_INTERMEDIATES_DIR) -name "*.ko" -print0 | xargs -0 -IX cp -v X $(NV_KERNEL_MODULES_TARGET_DIR)
 
 kernel-tests: kmodules FORCE
 	@echo "Kernel space tests build"
 	@echo "Tests at $(PRIVATE_TOPDIR)/vendor/nvidia/tegra/tests/linux/kernel_space_tests"
 	+$(hide) $(kernel-make) M=$(PRIVATE_TOPDIR)/vendor/nvidia/tegra/tests/linux/kernel_space_tests
-	find $(PRIVATE_TOPDIR)/vendor/nvidia/tegra/tests/linux/kernel_space_tests -name "*.ko" -print0 | xargs -0 -IX cp -v X $(TARGET_OUT)/lib/modules/
+	find $(PRIVATE_TOPDIR)/vendor/nvidia/tegra/tests/linux/kernel_space_tests -name "*.ko" -print0 | xargs -0 -IX cp -v X $(NV_KERNEL_MODULES_TARGET_DIR)
 	find $(PRIVATE_TOPDIR)/vendor/nvidia/tegra/tests/linux/kernel_space_tests -name "*.sh" -print0 | xargs -0 -IX cp -v X $(TARGET_OUT)/bin/
 	+$(hide) $(kernel-make) M=$(PRIVATE_TOPDIR)/vendor/nvidia/tegra/tests/linux/kernel_space_tests clean
 	find $(PRIVATE_TOPDIR)/vendor/nvidia/tegra/tests/linux/kernel_space_tests -name "modules.order" -print0 | xargs -0 -IX rm -rf X
 
-# At this stage, BUILT_SYSTEMIMAGE in build/core/Makefile has not yet
-# been defined, so we cannot rely on it.
+# At this stage, BUILT_SYSTEMIMAGE in $TOP/build/core/Makefile has not
+# yet been defined, so we cannot rely on it.
 _systemimage_intermediates_kmodules := \
     $(call intermediates-dir-for,PACKAGING,systemimage)
 BUILT_SYSTEMIMAGE_KMODULES := $(_systemimage_intermediates_kmodules)/system.img
+NV_INSTALLED_SYSTEMIMAGE := $(PRODUCT_OUT)/system.img
 
 # Unless we hardcode the list of kernel modules, we cannot create
 # a proper dependency from systemimage to the kernel modules.
@@ -103,21 +121,34 @@ BUILT_SYSTEMIMAGE_KMODULES := $(_systemimage_intermediates_kmodules)/system.img
 # For now, let's rely on an explicit dependency.
 $(BUILT_SYSTEMIMAGE_KMODULES): kmodules
 
+# Following dependency is already defined in $TOP/build/core/Makefile,
+# but for the sake of clarity let's re-state it here. This dependency
+# causes following dependencies to be indirectly defined:
+#   $(NV_INSTALLED_SYSTEMIMAGE): kmodules $(BUILT_KERNEL_TARGET)
+# which will prevent too early creation of systemimage.
+$(NV_INSTALLED_SYSTEMIMAGE): $(BUILT_SYSTEMIMAGE_KMODULES)
+
 $(INSTALLED_KERNEL_TARGET): $(BUILT_KERNEL_TARGET) | $(ACP)
 	$(copy-file-to-target)
 
-kernel: $(INSTALLED_KERNEL_TARGET) kmodules
+# Kernel build also includes some drivers as kernel modules which are
+# packaged inside system image. Therefore, for incremental builds,
+# dependency from kernel to installed system image must be introduced,
+# so that recompilation of kernel automatically updates also the
+# drivers in system image to be flashed to the device.
+kernel: $(INSTALLED_KERNEL_TARGET) kmodules $(NV_INSTALLED_SYSTEMIMAGE)
 
-kernel-%:
-	@mkdir -p $(PRIVATE_KBUILD_OUT)
+kernel-%: | $(NV_KERNEL_INTERMEDIATES_DIR)
 	$(hide) $(kernel-make) $*
+
+$(NV_KERNEL_INTERMEDIATES_DIR) $(NV_KERNEL_MODULES_TARGET_DIR):
+	$(hide) mkdir -p $@
 
 .PHONY: kernel kernel-% kernel-tests kmodules
 
 # Set private variables for all builds. TODO: Why?
 kernel kernel-% kernel-tests kmodules $(dotconfig) $(BUILT_KERNEL_TARGET): PRIVATE_SRC_PATH := $(KERNEL_PATH)
-kernel kernel-% kernel-tests kmodules $(dotconfig) $(BUILT_KERNEL_TARGET): PRIVATE_KBUILD_OUT := $(TARGET_OUT_INTERMEDIATES)/KERNEL
-kernel kernel-% kernel-tests kmodules $(dotconfig) $(BUILT_KERNEL_TARGET): PRIVATE_TOPDIR := $(shell pwd)
+kernel kernel-% kernel-tests kmodules $(dotconfig) $(BUILT_KERNEL_TARGET): PRIVATE_TOPDIR := $(CURDIR)
 
 endif
 # of ifneq ($(TARGET_NO_KERNEL),true)
