83191ad04fd267f8b2c636c3839371a41271a4fa
varun colbert <vcolbert@nvidia.com>
2011-12-08 15:35:53 -0800

    Make sure LOCAL_SRC_FILES don't contain ../ in the path
    Change-Id: I5c947454100a0c819f812ee1fc5d95cad3b46f5c
    Reviewed-on: http://git-master/r/69071
    Reviewed-by: Varun Colbert <vcolbert@nvidia.com>
    Tested-by: Varun Colbert <vcolbert@nvidia.com>

--- base.mk
+++ base.mk
@@ -5,10 +5,9 @@ endif
 NVIDIA_CLEARED := false
 
 # Make sure that LOCAL_SRC_FILES don't contain files with ../ in the path
-# devics is not yet ready for this yet
-#ifneq ($(findstring ../, $(dir $(LOCAL_SRC_FILES))),)
-#$(error $(LOCAL_PATH): ../ in path is not allowed for LOCAL_SRC_FILES)
-#endif
+ifneq ($(findstring ../, $(dir $(LOCAL_SRC_FILES))),)
+$(error $(LOCAL_PATH): ../ in path is not allowed for LOCAL_SRC_FILES)
+endif
 
 # output directory for generated files
 
