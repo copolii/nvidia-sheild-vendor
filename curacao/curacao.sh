f5f6177a462c22b54e24c9344dbf0552eb4ab839
Jay Agarwal <jagarwal@nvidia.com>
2012-03-15 18:47:09 +0530

    flash: Change odmdata to enable UART prints
    Modified odmdata for curacao platform to enable
    prints on UART Console.
    
    Bug 952266
    
    Change-Id: I4f97a94667ba08ca8effb857fde4068e39b86744
    Reviewed-on: http://git-master/r/90334
    Tested-by: Jay Agarwal <jagarwal@nvidia.com>
    Reviewed-by: Vikram Kanigiri <vkanigiri@nvidia.com>
    Reviewed-by: Vivek Kumar <vivekk@nvidia.com>
    Reviewed-by: Automatic_Commit_Validation_User
    Reviewed-by: Scott Williams <scwilliams@nvidia.com>
    Original: b925009b0a81c5ce54ffc74738eab55be95b4d2a

--- curacao/curacao.sh
+++ curacao/curacao.sh
@@ -12,12 +12,12 @@ then
     echo "Setting up NvFlash BCT for Curacao with DDR3 SDRAM......"
     cp $TEGRA_TOP/customers/nvidia/curacao/nvflash/curacao_13Mhz_H5TQ1G83BFR-H9C_13Mhz_1GB_emmc_H26M42001EFR_x8.bct $TOP/$OUTDIR/flash.bct
     cp $TEGRA_TOP/customers/nvidia/curacao/nvflash/android_fastboot_emmc_full.cfg                                   $TOP/$OUTDIR/flash.cfg
-    export NVFLASH_ODM_DATA=0x40080105
+    export NVFLASH_ODM_DATA=0x400c0105
 else
     echo "Setting up NvFlash BCT for Curacao with LPDDR2 SDRAM......"
     cp $TEGRA_TOP/customers/nvidia/curacao/nvflash/curacao_13Mhz_H8TBR00Q0MLR_13Mhz_256MB_emmc_H26M42001EFR_x8.bct $TOP/$OUTDIR/flash.bct
     cp $TEGRA_TOP/customers/nvidia/curacao/nvflash/android_fastboot_emmc_full.cfg                                  $TOP/$OUTDIR/flash.cfg
-    export NVFLASH_ODM_DATA=0x10080105
+    export NVFLASH_ODM_DATA=0x100c0105
 fi
 
 cp $TOP/$OUTDIR/obj/EXECUTABLES/bootloader_intermediates/bootloader.bin $TOP/$OUTDIR/bootloader.bin
