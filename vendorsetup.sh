ef0f251bac838b6c8c1fd159e8f313098dee739e
Antti P Miettinen <amiettinen@nvidia.com>
2012-04-27 14:01:17 +0300

    kernel: Stop creating directories named cd
    Add missing break between commands.
    
    Change-Id: If6f332eff23bc6738191cd9e443e326f0f3c3e5c
    Reviewed-on: http://git-master/r/99383
    Reviewed-by: Oskari Jaaskelainen <oskarij@nvidia.com>
    Reviewed-by: Konsta Holtta <kholtta@nvidia.com>
    Reviewed-by: Antti Miettinen <amiettinen@nvidia.com>
    Tested-by: Antti Miettinen <amiettinen@nvidia.com>
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Stefan Becker <stefanb@nvidia.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -212,7 +212,7 @@ function krebuild()
     fi
 
     (mkdir -p $T/$OUTDIR/modules \
-        cd $T && make modules_install -C $SRC $KARCH $CROSS $KOUT INSTALL_MOD_PATH=$T/$OUTDIR/modules \
+        && cd $T && make modules_install -C $SRC $KARCH $CROSS $KOUT INSTALL_MOD_PATH=$T/$OUTDIR/modules \
         && mkdir -p $T/$OUTDIR/system/lib/modules && cp -f `find $T/$OUTDIR/modules -name *.ko` $T/$OUTDIR/system/lib/modules \
         && $MKBOOTIMG --kernel $ZIMAGE --ramdisk $RAMDISK --output $T/$OUTDIR/boot.img )
 
