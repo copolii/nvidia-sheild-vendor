45d8b366c83009d64995af56c2205da45b9210c5
Tom Cherry <tcherry@nvidia.com>
2012-06-15 15:21:31 -0700

    build: add patches to apply_patches
    Add patches for:
    1) Wifi/Bt support
    2) Java build break
    3) Graphics_submit fix
    
    Change-Id: I5d61d95e2d860c8019d2ac58178575cfc5185c32
    Reviewed-on: http://git-master/r/109330
    Tested-by: Thomas Cherry <tcherry@nvidia.com>
    Reviewed-by: Maria Bornski <mbornski@nvidia.com>

--- apply_patches.sh
+++ apply_patches.sh
@@ -4,14 +4,12 @@ if [ a$GERRIT_USER == a ]; then
   export GERRIT_USER=$USER
 fi
 
-if [ a$TOP == a ]; then
-    echo \$TOP is not set. Please set \$TOP before running this script
-    exit 1
-else
-    echo === Changing to $TOP/system/core to apply patch
-    pushd $TOP/system/core
-    echo === git am $TOP/vendor/nvidia/build/0001-TEMPORARY-Re-add-LOG-variants.patch
-    git am $TOP/vendor/nvidia/build/0001-TEMPORARY-Re-add-LOG-variants.patch
+#directory, patchname
+function apply_patch {
+    echo === Changing to $TOP/$1 to apply patch
+    pushd $TOP/$1
+    echo === git am $TOP/vendor/nvidia/build/$2
+    git am $TOP/vendor/nvidia/build/$2
     if [ $? != 0 ]; then
         echo === error: Applying patch failed!
         echo === Aborting!
@@ -21,5 +19,17 @@ else
     fi
     echo === Restoring original directory
     popd
+}
+
+if [ a$TOP == a ]; then
+    echo \$TOP is not set. Please set \$TOP before running this script
+    exit 1
+else
+    apply_patch frameworks/native 0001-egl-Return-values-from-eglDestroyImageKHR.patch
+    apply_patch hardware/libhardware_legacy 0001-Enable-support-for-bcm-modules.patch
+    apply_patch build 0001-Fix-issue-with-DEBUG_OUT_DIR-not-properly-selected.patch
+    apply_patch system/netd 0001-softap-enable-bcm4329-support.patch
+    apply_patch system/core 0001-TEMPORARY-Re-add-LOG-variants.patch
+    apply_patch system/core 0002-set-ro.commchip-in-init.c.patch
 fi
 
