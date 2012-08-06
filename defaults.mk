8af687e90b758e38de0454eec7d62859bd96b108
Sandip Kumar <sandipk@nvidia.com>
2012-08-06 10:57:10 +0530

    build: retire TARGET_TEGRA_FAMILY
    TARGET_TEGRA_VERSION only uses values {t30,ap20} so there is no need for
    a separate TARGET_TEGRA_FAMILY variable, its basically a duplicate.
    
    Bug 972562
    
    Change-Id: Ic198a6b8a34b99d1c3a3a21ea35543b8df8d154d
    Reviewed-on: http://git-master/r/121179
    Reviewed-by: Automatic_Commit_Validation_User
    Tested-by: Sandip Kumar <sandipk@nvidia.com>
    Reviewed-by: Oskari Jaaskelainen <oskarij@nvidia.com>

--- defaults.mk
+++ defaults.mk
@@ -9,12 +9,12 @@ include $(CLEAR_VARS)
 # Build variables common to all nvidia modules
 
 LOCAL_C_INCLUDES += $(TEGRA_TOP)/core/include
-LOCAL_C_INCLUDES += $(TEGRA_TOP)/core/drivers/hwinc/$(TARGET_TEGRA_FAMILY)
+LOCAL_C_INCLUDES += $(TEGRA_TOP)/core/drivers/hwinc/$(TARGET_TEGRA_VERSION)
 LOCAL_C_INCLUDES += $(TEGRA_TOP)/core/drivers/hwinc
 LOCAL_C_INCLUDES += $(TEGRA_TOP)/camera/core/include
 LOCAL_C_INCLUDES += $(TEGRA_TOP)/multimedia-partner/openmax/include
 LOCAL_C_INCLUDES += $(TEGRA_TOP)/camera-partner/imager/include
-LOCAL_C_INCLUDES += $(TEGRA_TOP)/camera/core/hwinc/$(TARGET_TEGRA_FAMILY)
+LOCAL_C_INCLUDES += $(TEGRA_TOP)/camera/core/hwinc/$(TARGET_TEGRA_VERSION)
 LOCAL_C_INCLUDES += $(TEGRA_TOP)/camera/core/hwinc
 LOCAL_C_INCLUDES += $(TEGRA_TOP)/camera/core/camera
 LOCAL_C_INCLUDES += $(TEGRA_TOP)/core-private/include
@@ -25,7 +25,7 @@ LOCAL_C_INCLUDES += $(TEGRA_TOP)/multimedia-partner/utils/include
 LOCAL_C_INCLUDES += $(TEGRA_TOP)/multimedia-partner/nvmm/include
 ifneq (,$(findstring core-private,$(LOCAL_PATH)))
 LOCAL_C_INCLUDES += $(TEGRA_TOP)/core-private/include
-LOCAL_C_INCLUDES += $(TEGRA_TOP)/core-private/drivers/hwinc/$(TARGET_TEGRA_FAMILY)
+LOCAL_C_INCLUDES += $(TEGRA_TOP)/core-private/drivers/hwinc/$(TARGET_TEGRA_VERSION)
 LOCAL_C_INCLUDES += $(TEGRA_TOP)/core-private/drivers/hwinc
 endif
 
