19ebd3dfc4adaf757bf02b6cc59250f25c8b8bb9
Varun Varshney <vvarshney@nvidia.com>
2012-09-18 17:58:45 +0530

    nvflash: replace flash.bct with correct bct
    -Replace flash.bct with the required bct
    for pluto and dalmore(e1611 & e1613)
    -Make E1613 and E1612 as default
    
    Reviewed-on: http://git-master/r/133514
    (cherry picked from commit fcdcda4af64060d59a2325528d0cd1f81edbebaa)
    
    Change-Id: Iac03dfdf9a96e5d496cc821782b9e4a753c0256a
    Reviewed-on: http://git-master/r/140929
    Reviewed-by: Automatic_Commit_Validation_User
    Reviewed-by: Bharat Nihalani <bnihalani@nvidia.com>
    Tested-by: Bharat Nihalani <bnihalani@nvidia.com>

--- pluto/pluto.sh
+++ pluto/pluto.sh
@@ -1,10 +1,28 @@
-# NVIDIA Tegra3 "Pluto" development system
+#
+# Copyright (c) 2012, NVIDIA CORPORATION.  All rights reserved.
+#
+# NVIDIA Corporation and its licensors retain all intellectual property
+# and proprietary rights in and to this software, related documentation
+# and any modifications thereto.  Any use, reproduction, disclosure or
+# distribution of this software and related documentation without an express
+# license agreement from NVIDIA Corporation is strictly prohibited.
+#
+
+# NVIDIA Tegra4 "Pluto" development system
+OUTDIR=$(get_abs_build_var PRODUCT_OUT)
+echo "DEBUG: PRODUCT_OUT = $OUTDIR"
 
 # setup FASTBOOT VENDOR ID
 export FASTBOOT_VID=0x955
 if [ "$ODMDATA_OVERRIDE" ]; then
     export NVFLASH_ODM_DATA=$ODMDATA_OVERRIDE
 else
-    export NVFLASH_ODM_DATA=0x20098000
+    export NVFLASH_ODM_DATA=0x80098000
 fi
 
+if [ "$T30_ON_T114" == "1" ]
+then
+        cp $OUTDIR/flash_noxusb.cfg $OUTDIR/flash.cfg
+else
+        cp $OUTDIR/flash_xusb.cfg $OUTDIR/flash.cfg
+fi
