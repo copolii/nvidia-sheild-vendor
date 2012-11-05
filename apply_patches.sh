221d620af1353041444271d3ba738b27f1889f91
Nikesh Oswal <noswal@nvidia.com>
2012-11-05 18:45:18 +0530

    audio: Merge Audio Features (FW changes) on rel17-MR1
    ULP/WFD/MC/USB/Dual-Audio/AC3-Passthrough/tinyalsa
    related Audio FrameWork Changes for MR1
    
    Change-Id: Ia19df3fce400c02efeaa1877145bb913a5e14056
    Reviewed-on: http://git-master/r/162784
    Reviewed-by: Automatic_Commit_Validation_User
    Reviewed-by: David Pearce <dpearce@nvidia.com>
    Tested-by: David Pearce <dpearce@nvidia.com>

--- apply_patches.sh
+++ apply_patches.sh
@@ -28,12 +28,16 @@ else
     apply_patch build 0001-Fix-issue-with-DEBUG_OUT_DIR-not-properly-selected.patch
     apply_patch build 0002-build-Fix-Argument-list-too-long-error.patch
     apply_patch build 0003-pdk-Support-AIDL-files-in-java-builds.patch
-    apply_patch external/tinyalsa 0001-tinyalsa-return-card-number-from-its-name-substring.patch
     apply_patch frameworks/native 0001-egl-Return-values-from-eglDestroyImageKHR.patch
     apply_patch frameworks/native 0002-frameworks-native-add-extra-dalvik-heap-configs.patch
     apply_patch hardware/libhardware_legacy 0001-Enable-support-for-bcm-modules.patch
     apply_patch system/core 0001-TEMPORARY-Re-add-LOG-variants.patch
     apply_patch system/core 0002-Enable-use-of-modem.-property-by-radio-user.patch
     apply_patch system/core 0003-Enable-use-of-ro.sf.lcd_density-prop-by-system-user.patch
+    apply_patch system/core 0001-audio-add-ulp-audio-format-query.patch
+    apply_patch frameworks/av 0001-AudioFlinger-Support-ULP-audio-on-Android-JB.patch
+    apply_patch hardware/libhardware_legacy 0001-audio-policy-Add-WFD-device-in-Audio-Policy-Manager-.patch
+    apply_patch hardware/libhardware 0001-usbaudio-Get-card-number-from-substring.patch
+    apply_patch external/tinyalsa 0001-external-tinyalsa-Enable-tinyalsa-apps-in-JB-build.patch
 fi
 
