c0fe3c3432cd89d00e49c5407fae0d952bcbcb89
Dan Willemsen <dwillemsen@nvidia.com>
2012-11-13 18:42:14 -0800

    patches: Remove submitted changes
    Change-Id: Icf1fba8d7341cbf69d282543bde1d709f7520220
    Reviewed-on: http://git-master/r/163427
    Reviewed-by: Automatic_Commit_Validation_User
    Reviewed-by: Dan Willemsen <dwillemsen@nvidia.com>
    Tested-by: Dan Willemsen <dwillemsen@nvidia.com>

--- apply_patches.sh
+++ apply_patches.sh
@@ -25,15 +25,6 @@ if [ a$TOP == a ]; then
     echo \$TOP is not set. Please set \$TOP before running this script
     exit 1
 else
-    apply_patch build 0001-Fix-issue-with-DEBUG_OUT_DIR-not-properly-selected.patch
-    apply_patch build 0002-build-Fix-Argument-list-too-long-error.patch
-    apply_patch build 0003-pdk-Support-AIDL-files-in-java-builds.patch
-    apply_patch frameworks/native 0001-egl-Return-values-from-eglDestroyImageKHR.patch
-    apply_patch frameworks/native 0002-frameworks-native-add-extra-dalvik-heap-configs.patch
-    apply_patch hardware/libhardware_legacy 0001-Enable-support-for-bcm-modules.patch
-    apply_patch system/core 0001-TEMPORARY-Re-add-LOG-variants.patch
-    apply_patch system/core 0002-Enable-use-of-modem.-property-by-radio-user.patch
-    apply_patch system/core 0003-Enable-use-of-ro.sf.lcd_density-prop-by-system-user.patch
     apply_patch system/core 0001-audio-add-ulp-audio-format-query.patch
     #apply_patch frameworks/av 0001-AudioFlinger-Support-ULP-audio-on-Android-JB.patch
     apply_patch hardware/libhardware_legacy 0001-audio-policy-Add-WFD-device-in-Audio-Policy-Manager-.patch
