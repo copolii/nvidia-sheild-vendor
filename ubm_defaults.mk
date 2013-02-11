b313035de8101faee03e02b6533dab6896634b74
Peng Du <pdu@nvidia.com>
2013-02-11 14:37:24 -0800

    build: enable T132 UBM tag
    Change-Id: Iba0e8cfaad77440453bb855ad60841a65f235e50
    Reviewed-on: http://git-master/r/200052
    Reviewed-by: Automatic_Commit_Validation_User
    Reviewed-by: Peng Du <pdu@nvidia.com>
    Tested-by: Peng Du <pdu@nvidia.com>
    Reviewed-by: Alexander Van Brunt <avanbrunt@nvidia.com>

--- ubm_defaults.mk
+++ ubm_defaults.mk
@@ -2,3 +2,11 @@
 # Remove once these flags have been cleaned up from the tree.
 # NVUB_SUPPORTS_TXXX = 1
 # LOCAL_CFLAGS += -DNVUB_SUPPORTS_TXXX=1
+
+# Temporary define to enable T13X code
+NVUB_SUPPORTS_T132 ?= 1
+ifeq ($(NVUB_SUPPORTS_T132),1)
+LOCAL_CFLAGS += -DNVUB_SUPPORTS_T132=1
+else
+LOCAL_CFLAGS += -DNVUB_SUPPORTS_T132=0
+endif
