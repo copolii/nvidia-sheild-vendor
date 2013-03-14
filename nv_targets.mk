f7fc094704a6d992cfa5e12f4a0b7e74d782b941
Peng Du <pdu@nvidia.com>
2013-03-13 18:07:32 -0700

    build: remove redundant sim-image target
    Change-Id: I01b8b894ac8b65c79a4aa1ed5c0fe9523441a9ef
    Reviewed-on: http://git-master/r/209721
    Reviewed-by: Automatic_Commit_Validation_User
    Reviewed-by: Peng Du <pdu@nvidia.com>
    Tested-by: Peng Du <pdu@nvidia.com>
    Reviewed-by: Chao Xu <cxu@nvidia.com>
    Original: 125595994a199cf9e0c3e8f5ac6479e49a89f34b

--- nv_targets.mk
+++ nv_targets.mk
@@ -16,13 +16,6 @@ endif
 endif
 
 #
-# Generate ramdisk images for simulation
-#
-sim-image: nvidia-tests
-	device/nvidia/common/copy_simtools.sh
-	device/nvidia/common/generate_full_filesystem.sh
-
-#
 # bootloader blob target and macros
 #
 
