44ad70d12d8f5da84f7be5f3f40be4cedb8cd44c
Jean Huang <jeanh@nvidia.com>
2013-08-20 14:43:26 -0700

    Merge branch 'dev/jb-mr2-pdk/main' into main
    Change-Id: Ic75f88da5f294ef1c3dd9a95dfc8ff17732b0003
    Reviewed-on: http://git-master/r/262914
    Reviewed-by: Mandar Padmawar <mpadmawar@nvidia.com>
    Tested-by: Mandar Padmawar <mpadmawar@nvidia.com>

--- apply_patches.sh
+++ apply_patches.sh
@@ -4,6 +4,12 @@ if [ a$GERRIT_USER == a ]; then
   export GERRIT_USER=$USER
 fi
 
+export GENERATE=0
+
+if [ $1 == generate ]; then
+    export GENERATE=1
+fi
+
 #directory, patchname
 function apply_patch {
     echo === Changing to $TOP/$1 to apply patch
@@ -21,10 +27,62 @@ function apply_patch {
     popd
 }
 
+#directory, commitid
+function cherry_pick {
+    echo === Changing to $TOP/$1 to cherry pick
+    pushd $TOP/$1
+    if [ $GENERATE == 1 ]; then
+        echo === git format-patch $2
+        git fetch ssh://git-master.nvidia.com:12001/android/platform/$1 rel-17-partner
+        if [ $? != 0 ]; then
+            echo === error: Downloading patch failed!
+            echo === Aborting!
+            echo === Restoring original directory
+            popd
+            exit 1
+        fi
+        mkdir -p $TOP/vendor/nvidia/build/patches/$1/$2/
+        git format-patch -N -1 -o $TOP/vendor/nvidia/build/patches/$1/$2/ $2
+    else
+        echo === git am $TOP/vendor/nvidia/build/patches/$1/$2
+        git am $TOP/vendor/nvidia/build/patches/$1/$2/*.patch
+        if [ $? != 0 ]; then
+            echo === error: Applying patch failed!
+            echo === Aborting!
+            echo === Restoring original directory
+            popd
+            exit 1
+        fi
+    fi
+    echo === Restoring original directory
+    popd
+}
+
 if [ a$TOP == a ]; then
     echo \$TOP is not set. Please set \$TOP before running this script
     exit 1
 else
-    echo No patches to apply
+    
+    # Fix issue with DEBUG_OUT_DIR not properly selected
+    cherry_pick build f7dfb3689edcaf5f819fa5e691ce13abf858bca8
+    # pdk: Support AIDL files in java builds
+    cherry_pick build 3484c5a32d95ed1f8d4a7746e5f2f6559714630f
+    # enable NV_ANDROID_FRAMEWORKS_ENHANCEMENT flag
+    cherry-pick build 8e5c9b409eac0a2d56ea519ca35f749d590cd0a4
+    
+    # Enable support for bcm modules
+    cherry_pick hardware/libhardware_legacy e7678a06bc413e93f9cf612b9f38c7db5020e51b
+
+    # tinyalsa: return card number from its name substring
+    cherry_pick external/tinyalsa c3828fdc2ae1051bd2ca2c8e932f9ce6d61b0c19
+
+    # Enable use of ro.sf.lcd_density prop by system user
+    cherry_pick system/core 59e8f07a05b1f03a4d4802870420fe2fa0f225c2
+
+    # apply frameworks/native patches for powerservice.
+    apply_patch frameworks/native patches/frameworks/native/*
+
+    # appply hardware/libhardware patches for powerservice.
+    apply_patch hardware/libhardware patches/hardware/libhardware/*
 fi
 
--- detectversion.mk
+++ detectversion.mk
@@ -2,6 +2,10 @@ PLATFORM_IS_JELLYBEAN := 1
 ifneq (, $(findstring 4.2., $(PLATFORM_VERSION)))
 PLATFORM_IS_JELLYBEAN_MR1 := 1
 endif
+ifeq ($(PLATFORM_VERSION),4.3)
+PLATFORM_IS_JELLYBEAN_MR1 := 1
+PLATFORM_IS_JELLYBEAN_MR2 := 1
+endif
 
 ifneq (, $(findstring 3., $(PLATFORM_VERSION)))
   ifeq ($(BUILD_GOOGLETV),true)
