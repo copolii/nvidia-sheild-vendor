26923458be272cb122be7e50f033f8e8f2395f83
Sandip Kumar <sandipk@nvidia.com>
2013-08-28 06:13:16 -0700

    Revert "Fix borken dependency of static java libraries"
    This reverts commit cf1c11ca359c65337d52e168dd48b90aed337500.
    
    Change-Id: Id6663cd464761e9d7e49c80dab399c76229f6a74
    Reviewed-on: http://git-master/r/267366
    Reviewed-by: Sandip Kumar <sandipk@nvidia.com>
    GVS: Gerrit_Virtual_Submit
    Tested-by: Sandip Kumar <sandipk@nvidia.com>

--- static_java_library.mk
+++ static_java_library.mk
@@ -5,10 +5,3 @@ include $(BUILD_STATIC_JAVA_LIBRARY)
 
 # BUILD_JAVA_LIBRARY doesn't consider additional dependencies
 $(LOCAL_BUILT_MODULE): $(LOCAL_ADDITIONAL_DEPENDENCIES)
-
-# Somewhere dependency is broken for static java libraries in JB-MR2.
-# The recipe for ALL_MODULES.$(LOCAL_MODULE).BUILT target is not being
-# run and thus static java lib is not found at the location where it should
-# be. Making default target droidcore depend on $(LOCAL_BUILT_MODULE) so that
-# it gets copied in the location pointed by ALL_MODULES.$(LOCAL_MODULE).BUILT
-droidcore: $(LOCAL_BUILT_MODULE)
