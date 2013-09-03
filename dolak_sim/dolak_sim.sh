ad9ee50860a80aef9457d29d377781bddb354d2e
Matt Craighead <mcraighead@nvidia.com>
2013-09-03 13:30:10 -0500

    build: removed files for unsupported boards
    Change-Id: I306bc6607b713cce58216747dc59ed998e1b2102
    Reviewed-on: http://git-master/r/269637
    Reviewed-by: Automatic_Commit_Validation_User
    Reviewed-by: Matt Craighead <mcraighead@nvidia.com>
    Tested-by: Matt Craighead <mcraighead@nvidia.com>
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Allen Martin <amartin@nvidia.com>
    Reviewed-by: Bharat Nihalani <bnihalani@nvidia.com>

--- dolak_sim/dolak_sim.sh
+++
@@ -1,9 +0,0 @@
-# NVIDIA Tegra3.5 "dolak_sim" development system
-OUTDIR=$(get_build_var PRODUCT_OUT)
-
-# setup FASTBOOT VENDOR ID
-export FASTBOOT_VID=0x955
-
-# FIXME: Add NvFlash support
-
-cp $TOP/$OUTDIR/obj/EXECUTABLES/bootloader_intermediates/bootloader.bin $TOP/$OUTDIR/bootloader.bin
