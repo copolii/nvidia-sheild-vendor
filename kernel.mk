82a4bb26a3fc78a0b671cbe593961d0319af1210
Kerwin Wan <kerwinw@nvidia.com>
2013-12-12 13:41:07 +0800

    build: nvidia: add defconfig for t132
    Use tegra13_android_defconfig for t132 boards
    
    Bug 1425609
    
    Change-Id: Ie4be31f9ff8e4234a28658cda635b821d477a504
    Reviewed-on: http://git-master/r/344613
    Tested-by: Kerwin Wan <kerwinw@nvidia.com>
    Reviewed-by: Automatic_Commit_Validation_User
    Reviewed-by: Alexander Van Brunt <avanbrunt@nvidia.com>

--- kernel.mk
+++ kernel.mk
@@ -56,6 +56,8 @@ else ifeq ($(TARGET_TEGRA_VERSION),t148)
     TARGET_KERNEL_CONFIG ?= tegra14_android_defconfig
 else ifeq ($(TARGET_TEGRA_VERSION),t124)
     TARGET_KERNEL_CONFIG ?= tegra12_android_defconfig
+else ifeq ($(TARGET_TEGRA_VERSION),t132)
+    TARGET_KERNEL_CONFIG ?= tegra13_android_defconfig
 endif
 
 ifeq ($(wildcard $(KERNEL_PATH)/arch/$(REAL_TARGET_ARCH)/configs/$(TARGET_KERNEL_CONFIG)),)
