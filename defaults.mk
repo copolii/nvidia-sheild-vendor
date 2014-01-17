cd35b7aa0275a0fdc783288da20238288aee0453
Sumit Bhattacharya <sumitb@nvidia.com>
2014-01-17 15:37:05 +0530

    build: Add audio/include to default include paths
    Since audio code from /multimedia/codecs is moved to /multimedia/audio
    adding multimedia/audio/include to list of default NV include
    directories.
    
    Bug 1393082
    
    Change-Id: I6a80685985e57f651b332a77730ee2026ca9d926
    Reviewed-on: http://git-master/r/357072
    Tested-by: Sumit Bhattacharya <sumitb@nvidia.com>
    Reviewed-by: Automatic_Commit_Validation_User
    Reviewed-by: Sumit Bhattacharya <sumitb@nvidia.com>

--- defaults.mk
+++ defaults.mk
@@ -20,6 +20,7 @@ LOCAL_C_INCLUDES += $(TEGRA_TOP)/hwinc
 LOCAL_C_INCLUDES += $(TEGRA_TOP)/hwinc/$(TARGET_TEGRA_FAMILY)
 LOCAL_C_INCLUDES += $(TEGRA_TOP)/camera/core/include
 LOCAL_C_INCLUDES += $(TEGRA_TOP)/multimedia/codecs/include
+LOCAL_C_INCLUDES += $(TEGRA_TOP)/multimedia/audio/include
 LOCAL_C_INCLUDES += $(TEGRA_TOP)/multimedia/tvmr/include
 LOCAL_C_INCLUDES += $(TEGRA_TOP)/multimedia-partner/utils/include
 LOCAL_C_INCLUDES += $(TEGRA_TOP)/multimedia-partner/nvmm/include
