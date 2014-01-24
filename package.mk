0e6d5deb5afbf9e8d45b1e7ac0affe68ad4c9b7b
Dan Willemsen <dwillemsen@nvidia.com>
2014-01-23 21:33:51 -0800

    build: pdk: Disable packages that use unavailable libraries
    Change-Id: I67d844c549ac94c3a4bfa2cbef3c9189a025e82b
    Reviewed-on: http://git-master/r/359664
    Tested-by: Dan Willemsen <dwillemsen@nvidia.com>
    Reviewed-by: Dan Willemsen <dwillemsen@nvidia.com>

--- package.mk
+++ package.mk
@@ -1,3 +1,16 @@
+# Don't define the package if it uses android-support-* and we're building the PDK
+ifeq ($(TARGET_BUILD_PDK),true)
+ifneq ($(filter android-support-%,$(LOCAL_STATIC_JAVA_LIBRARIES)),)
+LOCAL_STATIC_JAVA_LIBRARIES := ignore
+$(warning Ignoring $(LOCAL_PACKAGE_NAME) because of dependency on android-support-*)
+endif
+ifneq ($(filter android-common,$(LOCAL_STATIC_JAVA_LIBRARIES)),)
+LOCAL_STATIC_JAVA_LIBRARIES := ignore
+$(warning Ignoring $(LOCAL_PACKAGE_NAME) because of dependency on android-common)
+endif
+endif
+
+ifneq ($(LOCAL_STATIC_JAVA_LIBRARIES),ignore)
 # Let NVIDIA_BASE know that this is a package
 LOCAL_MODULE_CLASS := APPS
 include $(NVIDIA_BASE)
@@ -6,3 +19,4 @@ include $(BUILD_PACKAGE)
 
 # BUILD_PACKAGE doesn't consider additional dependencies
 $(LOCAL_BUILT_MODULE): $(LOCAL_ADDITIONAL_DEPENDENCIES)
+endif
