88353c9928e972b5a85f132779fc8cf5883f3ff7
Dan Willemsen <dwillemsen@nvidia.com>
2014-02-05 20:18:46 -0800

    nvmake: Support multilib builds
    bug 1465011
    
    Change-Id: I5cd3341e66d9632c08e561fddd3187db046a93b4
    Reviewed-on: http://git-master/r/364198
    Reviewed-by: Automatic_Commit_Validation_User
    Reviewed-by: Antoine Chauveau <achauveau@nvidia.com>
    Tested-by: Dan Willemsen <dwillemsen@nvidia.com>
    Reviewed-by: Dan Willemsen <dwillemsen@nvidia.com>

--- nvmake_internal.mk
+++ nvmake_internal.mk
@@ -1,12 +1,32 @@
+ifeq ($(LOCAL_MODULE_CLASS),SHARED_LIBRARIES)
+OVERRIDE_BUILT_MODULE_PATH := $($(LOCAL_2ND_ARCH_VAR_PREFIX)TARGET_OUT_INTERMEDIATE_LIBRARIES)
+endif
+
 NVIDIA_NVMAKE_ADDITIONAL_DEPENDENCIES := \
 	$(LOCAL_ADDITIONAL_DEPENDENCIES) \
-	$(foreach l,$(LOCAL_SHARED_LIBRARIES),$(TARGET_OUT_INTERMEDIATE_LIBRARIES)/$(l).so)
+	$(foreach l,$(LOCAL_SHARED_LIBRARIES),$($(LOCAL_2ND_ARCH_VAR_PREFIX)TARGET_OUT_INTERMEDIATE_LIBRARIES)/$(l).so)
 
+ifeq ($(TARGET_$(LOCAL_2ND_ARCH_VAR_PREFIX)ARCH),arm)
 NVIDIA_NVMAKE_TARGET_ABI := androideabi
+NVIDIA_NVMAKE_TARGET_ARCH := ARMv7
+else
+NVIDIA_NVMAKE_TARGET_ABI := androideabi
+NVIDIA_NVMAKE_TARGET_ARCH := AArch64
+endif
 
 NVIDIA_NVMAKE_MODULE := \
-    $(NVIDIA_NVMAKE_TOP)/$(LOCAL_NVIDIA_NVMAKE_BUILD_DIR)/_out/Android_ARMv7_$(NVIDIA_NVMAKE_TARGET_ABI)_$(NVIDIA_NVMAKE_BUILD_TYPE)/$(NVIDIA_NVMAKE_MODULE_PRIVATE_PATH)/$(NVIDIA_NVMAKE_MODULE_NAME)$(LOCAL_MODULE_SUFFIX)
+    $(NVIDIA_NVMAKE_TOP)/$(LOCAL_NVIDIA_NVMAKE_BUILD_DIR)/_out/Android_$(NVIDIA_NVMAKE_TARGET_ARCH)_$(NVIDIA_NVMAKE_TARGET_ABI)_$(NVIDIA_NVMAKE_BUILD_TYPE)/$(NVIDIA_NVMAKE_MODULE_PRIVATE_PATH)/$(NVIDIA_NVMAKE_MODULE_NAME)$(LOCAL_MODULE_SUFFIX)
 
+#
+# Bring module from the nvmake build output, and apply the usual
+# processing for shared library or executable.
+#
+
+include $(BUILD_SYSTEM)/dynamic_binary.mk
+
+$(linked_module): $(NVIDIA_NVMAKE_MODULE) | $(ACP)
+	@echo "Copy from nvmake output: $(PRIVATE_MODULE) ($@)"
+	$(copy-file-to-target)
 
 #
 # Call into the nvmake build system to build the module
@@ -14,7 +34,7 @@ NVIDIA_NVMAKE_MODULE := \
 # Add NVUB_SUPPORTS_TXXX=1 to temporarily enable a chip
 #
 
-$(NVIDIA_NVMAKE_MODULE) $(LOCAL_MODULE)_nvmakeclean: NVIDIA_NVMAKE_COMMON_BUILD_PARAMS := \
+$(NVIDIA_NVMAKE_MODULE) $(my_register_name)_nvmakeclean: NVIDIA_NVMAKE_COMMON_BUILD_PARAMS := \
     TEGRA_TOP=$(TEGRA_TOP) \
     ANDROID_BUILD_TOP=$(ANDROID_BUILD_TOP) \
     OUT=$(OUT) \
@@ -23,24 +43,24 @@ $(NVIDIA_NVMAKE_MODULE) $(LOCAL_MODULE)_nvmakeclean: NVIDIA_NVMAKE_COMMON_BUILD_
     NV_HOST_OS=Linux \
     NV_HOST_ARCH=x86 \
     NV_TARGET_OS=Android \
-    NV_TARGET_ARCH=ARMv7 \
+    NV_TARGET_ARCH=$(NVIDIA_NVMAKE_TARGET_ARCH) \
     NV_BUILD_TYPE=$(NVIDIA_NVMAKE_BUILD_TYPE) \
     NV_COVERAGE_ENABLED=$(NVIDIA_COVERAGE_ENABLED) \
-    TARGET_TOOLS_PREFIX=$(abspath $(TARGET_TOOLS_PREFIX)) \
-    TARGET_C_INCLUDES="$(foreach inc,external/stlport/stlport $(TARGET_C_INCLUDES) bionic system/core/libsync/include,$(abspath $(inc)))" \
-    TARGET_OUT_INTERMEDIATE_LIBRARIES=$(abspath $(TARGET_OUT_INTERMEDIATE_LIBRARIES)) \
-    TARGET_LIBGCC=$(TARGET_LIBGCC) \
+    TARGET_TOOLS_PREFIX=$(abspath $($(LOCAL_2ND_ARCH_VAR_PREFIX)TARGET_TOOLS_PREFIX)) \
+    TARGET_C_INCLUDES="$(foreach inc,external/stlport/stlport $($(LOCAL_2ND_ARCH_VAR_PREFIX)TARGET_C_INCLUDES) bionic system/core/libsync/include,$(abspath $(inc)))" \
+    TARGET_OUT_INTERMEDIATE_LIBRARIES=$(abspath $($(LOCAL_2ND_ARCH_VAR_PREFIX)TARGET_OUT_INTERMEDIATE_LIBRARIES)) \
+    TARGET_LIBGCC=$($(LOCAL_2ND_ARCH_VAR_PREFIX)TARGET_LIBGCC) \
     $(NVUB_SUPPORTS_FLAG_LIST) \
     $(NVIDIA_NVMAKE_VERBOSE) \
     $(LOCAL_NVIDIA_NVMAKE_ARGS)
 
 ifeq ($(NV_USE_UNIX_BUILD),1)
-  $(NVIDIA_NVMAKE_MODULE) $(LOCAL_MODULE)_nvmakeclean: NVIDIA_NVMAKE_COMMAND := \
+  $(NVIDIA_NVMAKE_MODULE) $(my_register_name)_nvmakeclean: NVIDIA_NVMAKE_COMMAND := \
     $(NVIDIA_NVMAKE_UNIX_BUILD_COMMAND) \
     --newdir $(NVIDIA_NVMAKE_TOP)/$(LOCAL_NVIDIA_NVMAKE_BUILD_DIR) \
     nvmake
 else
-  $(NVIDIA_NVMAKE_MODULE) $(LOCAL_MODULE)_nvmakeclean: NVIDIA_NVMAKE_COMMAND := \
+  $(NVIDIA_NVMAKE_MODULE) $(my_register_name)_nvmakeclean: NVIDIA_NVMAKE_COMMAND := \
     $(MAKE) \
     MAKE=$(shell which $(MAKE)) \
     LD_LIBRARY_PATH=$(NVIDIA_NVMAKE_LIBRARY_PATH) \
@@ -50,30 +70,23 @@ else
 endif
 
 # This target needs to be forced, nvmake will do its own dependency checking
-$(NVIDIA_NVMAKE_MODULE): $(call local-intermediates-dir)/import_includes $(NVIDIA_NVMAKE_ADDITIONAL_DEPENDENCIES) FORCE
+$(NVIDIA_NVMAKE_MODULE): $(intermediates)/import_includes $(NVIDIA_NVMAKE_ADDITIONAL_DEPENDENCIES) FORCE
 	@echo "Build with nvmake: $(PRIVATE_MODULE) ($@)"
 	+$(hide) $(NVIDIA_NVMAKE_COMMAND) $(NVIDIA_NVMAKE_COMMON_BUILD_PARAMS) ANDROID_IMPORT_INCLUDES="$(subst -I ,-I$(abspath $(TOP))/,$(shell cat $(PRIVATE_IMPORT_INCLUDES)))" MAKEFLAGS="$(MAKEFLAGS)"
 
-$(LOCAL_MODULE)_nvmakeclean:
+$(my_register_name)_nvmakeclean:
 	@echo "Clean nvmake build files: $(PRIVATE_MODULE)"
 	+$(hide) $(NVIDIA_NVMAKE_COMMAND) $(NVIDIA_NVMAKE_COMMON_BUILD_PARAMS) MAKEFLAGS="$(MAKEFLAGS)" clobber
 
-.PHONY: $(LOCAL_MODULE)_nvmakeclean
+.PHONY: $(my_register_name)_nvmakeclean
 
 #
-# Bring module from the nvmake build output, and apply the usual
-# processing for shared library or executable.
-# Also make the module's clean target descend into nvmake.
+# Make the module's clean target descend into nvmake.
 #
 
-include $(BUILD_SYSTEM)/dynamic_binary.mk
-
-$(linked_module): $(NVIDIA_NVMAKE_MODULE) | $(ACP)
-	@echo "Copy from nvmake output: $(PRIVATE_MODULE) ($@)"
-	$(copy-file-to-target)
-
-$(cleantarget):: $(LOCAL_MODULE)_nvmakeclean
+$(cleantarget):: $(my_register_name)_nvmakeclean
 
 NVIDIA_NVMAKE_MODULE :=
 NVIDIA_NVMAKE_TARGET_ABI :=
+NVIDIA_NVMAKE_TARGET_ARCH :=
 NVIDIA_NVMAKE_ADDITIONAL_DEPENDENCIES :=
--- nvmake_shared_library.mk
+++ nvmake_shared_library.mk
@@ -1,9 +1,48 @@
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
 LOCAL_MODULE_SUFFIX := $(TARGET_SHLIB_SUFFIX)
-OVERRIDE_BUILT_MODULE_PATH := $(TARGET_OUT_INTERMEDIATE_LIBRARIES)
+
+ifneq ($(LOCAL_MODULE_PATH),)
+ifneq ($(TARGET_2ND_ARCH),)
+$(warning $(LOCAL_MODULE): LOCAL_MODULE_PATH for shared libraries is unsupported in multiarch builds, use LOCAL_MODULE_RELATIVE_PATH instead)
+endif
+endif
+
+ifneq ($(LOCAL_UNSTRIPPED_PATH),)
+ifneq ($(TARGET_2ND_ARCH),)
+$(warning $(LOCAL_MODULE): LOCAL_UNSTRIPPED_PATH for shared libraries is unsupported in multiarch builds)
+endif
+endif
 
 include $(NVIDIA_NVMAKE_BASE)
 
+LOCAL_2ND_ARCH_VAR_PREFIX :=
+include $(BUILD_SYSTEM)/module_arch_supported.mk
+
+ifeq ($(my_module_arch_supported),true)
 include $(NVIDIA_NVMAKE_INTERNAL)
+endif
+
+ifdef TARGET_2ND_ARCH
+
+LOCAL_2ND_ARCH_VAR_PREFIX := $(TARGET_2ND_ARCH_VAR_PREFIX)
+include $(BUILD_SYSTEM)/module_arch_supported.mk
+
+ifeq ($(my_module_arch_supported),true)
+# Build for TARGET_2ND_ARCH
+OVERRIDE_BUILT_MODULE_PATH :=
+LOCAL_BUILT_MODULE :=
+LOCAL_INSTALLED_MODULE :=
+LOCAL_MODULE_STEM :=
+LOCAL_BUILT_MODULE_STEM :=
+LOCAL_INSTALLED_MODULE_STEM :=
+LOCAL_INTERMEDIATE_TARGETS :=
+
+include $(NVIDIA_NVMAKE_INTERNAL)
+
+endif
+
+LOCAL_2ND_ARCH_VAR_PREFIX :=
+
+endif # TARGET_2ND_ARCH
 
 include $(NVIDIA_NVMAKE_CLEAR)
