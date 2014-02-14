9c752e14b37461135cf885c1c0f9e31ab977ef54
Dan Willemsen <dwillemsen@nvidia.com>
2014-02-14 00:08:08 -0800

    detectversion: Refactor, require KK+
    This branch doesn't support anything older than KitKat anymore.
    
    Change-Id: I6a8c06c3870718d536848b48a841be85493ac901
    Reviewed-on: http://git-master/r/367622
    Reviewed-by: Automatic_Commit_Validation_User
    Tested-by: Dan Willemsen <dwillemsen@nvidia.com>
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Dan Willemsen <dwillemsen@nvidia.com>

--- detectversion.mk
+++ detectversion.mk
@@ -1,22 +1,14 @@
+ifneq (, $(filter 3.%, $(PLATFORM_VERSION)))
+$(error Unsupported PLATFORM_VERSION = $(PLATFORM_VERSION))
+endif
+ifneq (, $(findstring 4.2%, $(PLATFORM_VERSION)))
+$(error Unsupported PLATFORM_VERSION = $(PLATFORM_VERSION))
+endif
+ifneq (, $(findstring 4.3%, $(PLATFORM_VERSION)))
+$(error Unsupported PLATFORM_VERSION = $(PLATFORM_VERSION))
+endif
+
 PLATFORM_IS_JELLYBEAN := 1
-ifneq (, $(findstring 4.2., $(PLATFORM_VERSION)))
-PLATFORM_IS_JELLYBEAN_MR1 := 1
-endif
-ifeq ($(PLATFORM_VERSION),4.3)
-PLATFORM_IS_JELLYBEAN_MR1 := 1
-PLATFORM_IS_JELLYBEAN_MR2 := 1
-endif
-ifneq (, $(findstring 4.4, $(PLATFORM_VERSION)))
 PLATFORM_IS_JELLYBEAN_MR1 := 1
 PLATFORM_IS_JELLYBEAN_MR2 := 1
 PLATFORM_IS_KITKAT := 1
-endif
-
-ifneq (, $(findstring 3., $(PLATFORM_VERSION)))
-  ifeq ($(BUILD_GOOGLETV),true)
-     PLATFORM_IS_GTV_HC := 1
-     PLATFORM_IS_JELLYBEAN :=
-  endif
-endif
-
-$(warn ALERT platform is jellybean mr1 = $(PLATFORM_IS_JELLYBEAN_MR1))
