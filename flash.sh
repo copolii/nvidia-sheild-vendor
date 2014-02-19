81042cd8e03f6bc1e14f65fa5736dfe85294f11d
Vinayak Pane <vpane@nvidia.com>
2014-02-19 15:15:59 -0800

    flash: loki: repair foster a01 option
    Select Foster A01 board and suitable DTB file.
    
    Change-Id: I039a538f70c6420c837b02a9c09cc545089ec29a
    Reviewed-on: http://git-master/r/369525
    Reviewed-by: Vinayak Pane <vpane@nvidia.com>
    Tested-by: Vinayak Pane <vpane@nvidia.com>
    Reviewed-by: Automatic_Commit_Validation_User
    Reviewed-by: David Dastous St Hilaire <ddastoussthi@nvidia.com>
    Reviewed-by: Sachin Nikam <snikam@nvidia.com>
    GVS: Gerrit_Virtual_Submit

--- flash.sh
+++ flash.sh
@@ -214,7 +214,7 @@ loki() {
     [[ -n $BOARD_IS_LOKI_NFF_B00_2GB ]] && board=loki_nff_b00_2gb
     if [[ -z $board ]] && _shell_is_interactive; then
         # Prompt user for target board info
-        _choose "Which board to flash?" "e2548_a02 loki_nff_b00 loki_nff_b00_2gb thor_195 loki_ffd_prem loki_ffd_prem_a01 loki_ffd_prem_a03 loki_ffd_base loki_ffd_base_a1 loki_ffd_base_a3 foster_pro foster_pro_a1" board loki_nff_b00
+        _choose "Which board to flash?" "e2548_a02 loki_nff_b00 loki_nff_b00_2gb thor_195 loki_ffd_prem loki_ffd_prem_a01 loki_ffd_prem_a03 loki_ffd_base loki_ffd_base_a1 loki_ffd_base_a3 foster_pro foster_pro_a01" board loki_nff_b00
     else
         board=${board-loki_nff_b00}
     fi
@@ -237,7 +237,7 @@ loki() {
         odmdata=0x29c000
     elif [[ $board == foster_pro_a01 ]]; then
         nct="--nct NCT_foster_a1.txt"
-        l_dtbfile=tegra124-foster.dtb
+        dtbfile=tegra124-foster.dtb
         bctfile=bct_loki_ffd_sku0.cfg
         odmdata=0x29c000
     elif [[ $board == loki_ffd_prem ]]; then
