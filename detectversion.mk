8911611faf923a83445de48ec6a06bb6c369473c
Dan Willemsen <dwillemsen@nvidia.com>
2014-03-24 18:10:15 -0700

    build: Add PLATFORM_IS_NEXT
    Fix previous findstrings with filter.
    
    Change-Id: Ie8bfeef0eac4101a4151e275899a81a41fb39540
    Reviewed-on: http://git-master/r/385911
    Reviewed-by: Automatic_Commit_Validation_User
    Reviewed-by: Gaurav Sarode <gsarode@nvidia.com>
    Tested-by: Gaurav Sarode <gsarode@nvidia.com>
    Reviewed-by: Prajakta Gudadhe <pgudadhe@nvidia.com>

--- detectversion.mk
+++ detectversion.mk
@@ -1,10 +1,10 @@
 ifneq (, $(filter 3.%, $(PLATFORM_VERSION)))
 $(error Unsupported PLATFORM_VERSION = $(PLATFORM_VERSION))
 endif
-ifneq (, $(findstring 4.2%, $(PLATFORM_VERSION)))
+ifneq (, $(filter 4.2%, $(PLATFORM_VERSION)))
 $(error Unsupported PLATFORM_VERSION = $(PLATFORM_VERSION))
 endif
-ifneq (, $(findstring 4.3%, $(PLATFORM_VERSION)))
+ifneq (, $(filter 4.3%, $(PLATFORM_VERSION)))
 $(error Unsupported PLATFORM_VERSION = $(PLATFORM_VERSION))
 endif
 
@@ -12,3 +12,7 @@ PLATFORM_IS_JELLYBEAN := 1
 PLATFORM_IS_JELLYBEAN_MR1 := 1
 PLATFORM_IS_JELLYBEAN_MR2 := 1
 PLATFORM_IS_KITKAT := 1
+
+ifeq (, $(filter 4.4%, $(PLATFORM_VERSION)))
+PLATFORM_IS_NEXT := 1
+endif
