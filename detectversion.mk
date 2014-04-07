9b4374c4a018f3d3edab5b8a8c3b4b8c8e74d8d8
Dan Willemsen <dwillemsen@nvidia.com>
2014-04-07 08:47:57 -0700

    Revert "build: Add PLATFORM_IS_NEXT"
    This reverts commit 8911611faf923a83445de48ec6a06bb6c369473c.
    Not ready yet.
    
    Change-Id: I8126f53fb99db5b390914746fbc6daf5711b94bc
    Reviewed-on: http://git-master/r/392941
    Reviewed-by: Automatic_Commit_Validation_User
    Reviewed-by: Dan Willemsen <dwillemsen@nvidia.com>
    Tested-by: Dan Willemsen <dwillemsen@nvidia.com>

--- detectversion.mk
+++ detectversion.mk
@@ -1,10 +1,10 @@
 ifneq (, $(filter 3.%, $(PLATFORM_VERSION)))
 $(error Unsupported PLATFORM_VERSION = $(PLATFORM_VERSION))
 endif
-ifneq (, $(filter 4.2%, $(PLATFORM_VERSION)))
+ifneq (, $(findstring 4.2%, $(PLATFORM_VERSION)))
 $(error Unsupported PLATFORM_VERSION = $(PLATFORM_VERSION))
 endif
-ifneq (, $(filter 4.3%, $(PLATFORM_VERSION)))
+ifneq (, $(findstring 4.3%, $(PLATFORM_VERSION)))
 $(error Unsupported PLATFORM_VERSION = $(PLATFORM_VERSION))
 endif
 
@@ -12,7 +12,3 @@ PLATFORM_IS_JELLYBEAN := 1
 PLATFORM_IS_JELLYBEAN_MR1 := 1
 PLATFORM_IS_JELLYBEAN_MR2 := 1
 PLATFORM_IS_KITKAT := 1
-
-ifeq (, $(filter 4.4%, $(PLATFORM_VERSION)))
-PLATFORM_IS_NEXT := 1
-endif
